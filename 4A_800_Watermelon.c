579419a5005eac3c9b3743e19101435ce3284bc3
Dibbojit Das Joy <138796389+Dibbojit@users.noreply.github.com>
2023-07-22 12:51:48 +0600

    4A_800_Watermelon.c

---
+++ 4A_800_Watermelon.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int w;
+    scanf("%d", &w);
+    
+    if(w > 2)
+    {
+        if(w % 2 == 0)
+        {
+            printf("yes\n");
+        }
+        else
+        {
+            printf("no\n");
+        }
+        else
+        {
+            printf("no\n");
+        }
+    }
+    return 0;
+}
